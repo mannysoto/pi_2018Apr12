5d6095826d65c743498f6cfe86bc45e24a844433
Manuel Soto <mannysoto@yahoo.com>
2018-04-12 23:55:59 +0000

    Initial commit

---
+++ basicProgram.cs
@@ -0,0 +1,22 @@
+
+using System; 
+
+
+
+namespace MannySoto
+{
+   class ManuelSoto
+   {
+      public static void Main(string[] args)
+      {
+         Console.Write("Hello Manny Soto"); 
+      }
+   }
+
+}
+
+
+
+
+
+
